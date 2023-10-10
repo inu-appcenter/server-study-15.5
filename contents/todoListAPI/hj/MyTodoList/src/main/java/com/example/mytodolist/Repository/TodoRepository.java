67b1419da1cd228dd33a9e1db9d35fcaafba04fa
sukangpunch <popora99@naver.com>
2023-10-10 16:08:35 +0900

    [Feat] Entity객체와 Repository layer 구성 - #25

---
+++ contents/todoListAPI/hj/MyTodoList/src/main/java/com/example/mytodolist/Repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.mytodolist.Repository;
+
+import com.example.mytodolist.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+
+
+}
---
+++ contents/todoListAPI/hj/MyTodoList/src/main/java/com/example/mytodolist/domain/Time.java
@@ -0,0 +1,23 @@
+package com.example.mytodolist.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass //상위 클래스인 Time 클래스의 필드와 매핑정보를 하위 엔티티 클래스로 상속시키는 역할.
+@EntityListeners(value = {AuditingEntityListener.class}) //엔티티의 변화를 감지하는 리스너
+public abstract class Time {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedDate;
+}
