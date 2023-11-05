927ac3b953168edb318b0c8070da5bbf60856759
hen715 <hen715@naver.com>
2023-11-05 21:01:36 +0900

    [Feat] Controller&Service 구현

--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/repositoy/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.toDoList.repositoy;
-
-import com.study.toDoList.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/repositoy/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.toDoList.repositoy;
-
-import com.study.toDoList.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
