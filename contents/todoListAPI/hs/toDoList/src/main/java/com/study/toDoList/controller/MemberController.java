4cead6060f4db0b81a66d24df798932423678c8d
hen715 <hen715@naver.com>
2023-11-06 22:08:54 +0900

    [Refactor] 중복된 엔드포인트에 대한 처리, 복수명사 기반으로 api 엔드포인트 수정, 회원아이디를 받아 그 회원의 모든 테스크를 가져오는 기능 이동, ResponseEntity에 대한 일관성있는 패턴 사용 #61

--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/controller/MemberController.java
+++ contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.study.toDoList.dto.MemberSaveDto;
 import com.study.toDoList.dto.MemberUpdateDto;
 import com.study.toDoList.dto.ResponseDto;
 import com.study.toDoList.service.MemberService;
+import com.study.toDoList.service.TaskService;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,16 +18,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
+    private final TaskService taskService;
     @Operation(summary = "회원 가입",description = "바디에 {email,password,nickname}을 json 형식으로 보내주세요.")
     @ApiResponses({
             @ApiResponse(code = 201,message = "회원가입성공")
     })
-    @PostMapping("/api/member")
-    public ResponseEntity<?> join(@RequestBody MemberSaveDto memberSaveDto){
-        Long id = memberService.save(memberSaveDto);
+    @PostMapping("/")
+    public ResponseEntity<?> join(@RequestParam("email")String email,@RequestParam("password")String password,@RequestParam("nickname")String nickname){
+        Long id = memberService.save(email,password,nickname);
 
         return new ResponseEntity<>(new ResponseDto(id,"회원가입성공"), HttpStatus.CREATED);
     }
@@ -35,7 +38,7 @@ public class MemberController {
     @ApiResponses({
             @ApiResponse(code = 200,message = "회원정보수정성공")
     })
-    @PutMapping("/api/member/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody MemberUpdateDto memberUpdateDto){
         memberService.update(id,memberUpdateDto);
         return new ResponseEntity<>(new ResponseDto(id,"회원정보수정성공"), HttpStatus.OK);
@@ -44,7 +47,7 @@ public class MemberController {
     @ApiResponses({
             @ApiResponse(code = 204,message = "회원삭제성공")
     })
-    @DeleteMapping("/api/member/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
         memberService.delete(id);
         return new ResponseEntity<>(new ResponseDto(id,"회원삭제성공"), HttpStatus.NO_CONTENT);
@@ -54,8 +57,17 @@ public class MemberController {
     @ApiResponses({
             @ApiResponse(code = 200,message = "회원가져오기성공")
     })
-    @GetMapping("/api/member/{id}")
-    public ResponseEntity<MemberResponseDto> getMember(@PathVariable Long id){
-        return ResponseEntity.ok().body(memberService.getMember(id));
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getMember(@PathVariable Long id){
+        return new ResponseEntity<>(memberService.getMember(id),HttpStatus.OK);
+    }
+
+    @Operation(summary = "모든 할일 가져오기",description = "url 경로변수에 회원아이디를 담아 보내주세요")
+    @ApiResponses({
+            @ApiResponse(code = 200,message = "모든할일가져오기성공")
+    })
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<?> getAllTask(@PathVariable Long id){
+        return new ResponseEntity<>(taskService.getAllTask(id),HttpStatus.OK);
     }
 }
