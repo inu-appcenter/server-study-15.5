f2da0278d8b554f5ccae8b7f1e1f41db14af8fc4
jang-namu <ironhee8005@gmail.com>
2023-11-07 15:57:54 +0900

    [Feat] Task 전체 수정 엔드포인트 추가, 일부 엔드포인트 변경 '/tasks/iscompleted' -> '/tasks/completed'

--- contents/todoListAPI/ch/todo/src/main/java/com/example/todo/task/TaskController.java
+++ contents/todoListAPI/ch/todo/src/main/java/com/example/todo/task/TaskController.java
@@ -1,11 +1,10 @@
 package com.example.todo.task;
 
-import com.example.todo.task.dto.TaskDto;
+import com.example.todo.task.dto.TaskRequestDto;
 import com.example.todo.task.dto.TaskResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,24 +17,34 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<List<TaskResponseDto>> getTasksByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<TaskResponseDto>> getTasksByUserId(@PathVariable Long userId) throws Exception {
         List<TaskResponseDto> taskResponseDtos = taskService.getAllByUserId(userId);
         return ResponseEntity.status(HttpStatus.OK).body(taskResponseDtos);
     }
 
     @PostMapping()
-    public ResponseEntity<TaskResponseDto> postTask(@RequestBody TaskDto taskDto) throws Exception {
-        TaskResponseDto taskResponseDto = taskService.save(taskDto);
-        return ResponseEntity.status(HttpStatus.OK).body(taskResponseDto);
+    public ResponseEntity<TaskResponseDto> postTask(@RequestBody TaskRequestDto taskRequestDto) throws Exception {
+        TaskResponseDto taskResponseDto = taskService.save(taskRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskResponseDto);
     }
 
     @DeleteMapping("/{taskId}")
-    public void deletetask(@PathVariable Long taskId) throws Exception {
+    public void deleteTask(@PathVariable Long taskId) throws Exception {
         taskService.delete(taskId);
     }
 
-    @PatchMapping("/iscompleted/{taskId}")
-    public void patchIsCompletedTask(@PathVariable Long taskId) throws Exception {
+    @PatchMapping("/completed/{taskId}")
+    public void changeTaskIsCompleted(@PathVariable Long taskId) throws Exception {
         taskService.changeIsCompleted(taskId);
     }
+
+    @PutMapping()
+    public ResponseEntity<TaskResponseDto> updateTask(@RequestBody TaskRequestDto taskRequestDto) throws Exception {
+        TaskResponseDto taskResponseDto = taskService.updateTask(taskRequestDto);
+        /*
+        고민, HttpStatus를 NO_CONTENT와 CREATED로 나누고 싶은데 컨트롤러에서 어떻게 식별할 수 있을까
+         */
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).body(taskResponseDto);
+    }
+
 }
