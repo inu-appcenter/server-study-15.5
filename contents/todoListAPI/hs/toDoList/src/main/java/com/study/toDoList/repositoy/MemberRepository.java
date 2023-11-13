3a25de7640f4c29f47763aff55b65acca8dd10f5
hen715 <hen715@naver.com>
2023-11-13 17:25:09 +0900

    [Feat] 이메일로 유저 찾기 추가

--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/repositoy/MemberRepository.java
+++ contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/repositoy/MemberRepository.java
@@ -3,5 +3,8 @@ package com.study.toDoList.repositoy;
 import com.study.toDoList.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
 }
