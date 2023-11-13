536a1a204734582ae9cf25efdd0b5c05d613e883
hen715 <hen715@naver.com>
2023-11-13 17:25:31 +0900

    [Feat] 예외처리 추가 #74

--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/service/MemberService.java
+++ contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/service/MemberService.java
@@ -4,6 +4,9 @@ import com.study.toDoList.domain.Member;
 import com.study.toDoList.dto.MemberResponseDto;
 import com.study.toDoList.dto.MemberSaveDto;
 import com.study.toDoList.dto.MemberUpdateDto;
+import com.study.toDoList.exception.ex.MyErrorCode;
+import com.study.toDoList.exception.ex.MyNotFoundException;
+import com.study.toDoList.exception.ex.MyNotValidException;
 import com.study.toDoList.repositoy.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,25 +24,28 @@ public class MemberService {
     @Transactional
     public Long save(String email, String password,String nickname){
         /*받은 비밀번호를 암호화*/
+        if(memberRepository.findByEmail(email).isPresent()){
+            throw new MyNotValidException(MyErrorCode.USER_NOT_VAlID);
+        }
         return memberRepository.save(MemberSaveDto.builder().email(email).password(password).nickname(nickname).build().toEntity()).getId();
     }
 
     @Transactional
     public Long update(Long id, MemberUpdateDto memberUpdateDto){
-        Optional<Member> member = memberRepository.findById(id);
-        member.get().update(memberUpdateDto.getPassword(),memberUpdateDto.getNickname());
+        Member member = memberRepository.findById(id).orElseThrow(()->new MyNotFoundException(MyErrorCode.USER_NOT_FOUND));
+        member.update(memberUpdateDto.getPassword(),memberUpdateDto.getNickname());
         return id;
     }
 
     @Transactional
     public void delete(Long id){
-        Optional<Member> member = memberRepository.findById(id);
-        memberRepository.delete(member.get());
+        Member member = memberRepository.findById(id).orElseThrow(()-> new MyNotFoundException(MyErrorCode.USER_NOT_FOUND));
+        memberRepository.delete(member);
     }
 
     @Transactional(readOnly = true)
     public MemberResponseDto getMember(Long id){
-        Optional<Member> member = memberRepository.findById(id);
-        return new MemberResponseDto(member.get());
+        Member member = memberRepository.findById(id).orElseThrow(()-> new MyNotFoundException(MyErrorCode.USER_NOT_FOUND));
+        return new MemberResponseDto(member);
     }
 }
