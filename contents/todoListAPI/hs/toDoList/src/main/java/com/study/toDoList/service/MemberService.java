0d89fa9f8c295f07ba5e3619f05f898da6051a02
hen715 <hen715@naver.com>
2023-11-15 10:57:47 +0900

    [Refactor] save에서 받는 인자 Dto로 변경

--- contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/service/MemberService.java
+++ contents/todoListAPI/hs/toDoList/src/main/java/com/study/toDoList/service/MemberService.java
@@ -4,15 +4,14 @@ import com.study.toDoList.domain.Member;
 import com.study.toDoList.dto.MemberResponseDto;
 import com.study.toDoList.dto.MemberSaveDto;
 import com.study.toDoList.dto.MemberUpdateDto;
+import com.study.toDoList.exception.ex.MyDuplicateException;
 import com.study.toDoList.exception.ex.MyErrorCode;
 import com.study.toDoList.exception.ex.MyNotFoundException;
-import com.study.toDoList.exception.ex.MyNotValidException;
 import com.study.toDoList.repositoy.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +21,12 @@ public class MemberService {
     //private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Long save(String email, String password,String nickname){
-        /*받은 비밀번호를 암호화*/
-        /*if(memberRepository.findByEmail(email).isPresent()){
-            throw new MyNotValidException(MyErrorCode.USER_NOT_VAlID);
-        }*/
-        return memberRepository.save(MemberSaveDto.builder().email(email).password(password).nickname(nickname).build().toEntity()).getId();
+    public Long save(MemberSaveDto memberSaveDto){
+        String encodedPassword = memberSaveDto.getPassword();/*받은 비밀번호 암호화*/
+        if(memberRepository.existsByEmail(memberSaveDto.getEmail())){
+            throw new MyDuplicateException(MyErrorCode.USER_DUPLICATE_EMAIL);
+        }
+        return memberRepository.save(MemberSaveDto.builder().email(memberSaveDto.getEmail()).password(encodedPassword).nickname(memberSaveDto.getNickname()).build().toEntity()).getId();
     }
 
     @Transactional
