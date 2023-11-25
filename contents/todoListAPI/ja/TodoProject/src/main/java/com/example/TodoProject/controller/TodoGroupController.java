971b7c5cd420aa1a4885ed3ffbd34ca5a67dbfbb
김정아 <92552047+squidjiny@users.noreply.github.com>
2023-11-25 17:37:10 +0900

    [Refactor] todoGroup 컨트롤러 url 대문자 변경, delete url @pathvariable 일부 삭제

--- contents/todoListAPI/ja/TodoProject/src/main/java/com/example/TodoProject/controller/TodoGroupController.java
+++ contents/todoListAPI/ja/TodoProject/src/main/java/com/example/TodoProject/controller/TodoGroupController.java
@@ -19,7 +19,7 @@ import javax.validation.Valid;
 import static com.example.TodoProject.dto.TodoGroup.TodoGroupRequestDto.*;
 import static com.example.TodoProject.dto.TodoGroup.TodoGroupResponseDto.*;
 @RestController
-@RequestMapping("/todoGroup")
+@RequestMapping("/todogroup")
 public class TodoGroupController {
 
     /*
@@ -93,14 +93,14 @@ public class TodoGroupController {
     }
 
     @Operation(summary = "투두 그룹 삭제", description = "투두 그룹을 삭제합니다. <br><br> 입력: <br> 1. 삭제하려는 투두 그룹 소유자의 데이터베이스 상 pk(clientnum).<br> 2. 삭제하려는 투두그룹의 데이터베이스 상 pk(todogorupnum)<br><br> 출력: data에 null 전달 ")
-    @DeleteMapping("/{clientnum}/{todogroupnum}")
+    @DeleteMapping("/{todogroupnum}")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "투두 삭제 성공"),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자입니다."),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 투두 그룹입니다."),
             @ApiResponse(responseCode = "403", description = "투두 그룹의 소유주가 아닙니다.")
     })
-    public ResponseEntity<CommonResponseDto> deleteTodoGroup(@PathVariable Long clientnum, @PathVariable Long todogroupnum){
+    public ResponseEntity<CommonResponseDto> deleteTodoGroup(Long clientnum, @PathVariable Long todogroupnum){
         todoGroupService.deleteTodoGroup(clientnum,todogroupnum);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new CommonResponseDto(CommonResponse.SUCCESS, "투두 그룹 삭제 성공",null));
