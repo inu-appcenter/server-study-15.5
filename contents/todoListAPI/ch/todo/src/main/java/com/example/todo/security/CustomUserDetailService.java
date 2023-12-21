9b6e001acdd96ba196f25e60563b7d327751f5ff
jang-namu <ironhee8005@gmail.com>
2023-12-21 14:52:52 +0900

    [Feat] CustomUserDetailSerivce class UserDetailService implementation

---
+++ contents/todoListAPI/ch/todo/src/main/java/com/example/todo/security/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.example.todo.security;
+
+import com.example.todo.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new CustomUserDetails(userRepository.findByLoginId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("일치하는 User를 찾을 수 없습니다."))
+        );
+    }
+}
