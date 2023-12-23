388f091064a6ba8412a18de8cdf2f994b226a78a
jang-namu <ironhee8005@gmail.com>
2023-12-23 21:53:08 +0900

    [Feat] Swagger에 Spring Security(jwt) 적용

--- contents/todoListAPI/ch/todo/src/main/java/com/example/todo/config/SwaggerConfiguration.java
+++ contents/todoListAPI/ch/todo/src/main/java/com/example/todo/config/SwaggerConfiguration.java
@@ -4,15 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -30,8 +36,36 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage("com.example.todo"))
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                // Swagger에서 Jwt를 인증방식으로 사용. 페이지에 Authorize 버튼을 만든다.
+                .securitySchemes(Arrays.asList(apiKey()))
+                // 전역적인 인가를 위한 Jwt security context 명세
+                .securityContexts(Arrays.asList(securityContext()));
     }
+
+    private ApiKey apiKey() {
+        // second arg is header name
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -41,6 +75,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
                 .build();
     }
 
+    /*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
@@ -48,5 +83,5 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
+     */
 }
